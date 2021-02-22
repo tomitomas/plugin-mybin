828622bed053287d4d46a4e7511b72a0d8accc0d
hugoKs3 <hugo@questroy.com>
2021-02-22 17:54:10 +0100

    continue

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -21,3 +21,25 @@ if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
+
+<form class="form-horizontal">
+	<fieldset>
+		<legend>
+			<i class="fa fa-list-alt"></i> {{Paramètres}}
+		</legend>
+        <div class="form-group">
+			<input class="configKey form-control" data-l1key="myttscmd"/>
+            <span class="input-group-btn">
+                <a class="btn btn-default cursor" title="Rechercher une commande" id="mymodal"><i class="fas fa-list-alt"></i></a>
+            </span>
+		</div>
+	</fieldset>
+</form>
+
+<script>
+$('#mymodal').on('click', function () {
+    jeedom.cmd.getSelectModal({cmd: {type: 'action'}}, function(result) {
+        $('.eqLogicAttr[data-l1key=myttscmd]').value(result.human);
+    });
+});
+</script>
