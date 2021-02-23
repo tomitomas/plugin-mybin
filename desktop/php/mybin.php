aa90d5bd1506cd94aeac986151cdba899bd87cba
hugoKs3 <hugo@questroy.com>
2021-02-23 16:25:16 +0100

    4 bins

--- desktop/php/mybin.php
+++ desktop/php/mybin.php
@@ -62,7 +62,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<br/>
 				<form class="form-horizontal">
 					<fieldset>
-						<div>
+						<div style="width: 100%; display:inline-block;">
                         <div class="col-lg-6">
 							<legend><i class="fas fa-wrench"></i> {{Général}}</legend>
 							<div class="form-group">
