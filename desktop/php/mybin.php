1c98d370122c617bf1782910ef188ab181fdb71d
hugoKs3 <hugo@questroy.com>
2021-02-26 21:34:43 +0100

    new design

--- desktop/php/mybin.php
+++ desktop/php/mybin.php
@@ -219,32 +219,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							</div>
 							<br>
 						</div>
-
-						<div class="col-lg-5">
-							<legend><i class="icon jeedomapp-preset"></i> {{Options}}</legend>
-							<div class="form-group">
-								<label class="col-sm-4 control-label">{{Template de widget}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Cocher la case pour utiliser le template de widget}}"></i></sup>
-								</label>
-								<div class="col-sm-1">
-									<input type="checkbox" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="widgetTemplate"/>
-								</div>
-							</div>
-                            <!--
-                            <div class="form-group">
-								<label class="col-sm-4 control-label">{{Commande TTS}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Commande TTS à exécuter lorsque qu'il faut sortir les poubelles}}"></i></sup>
-								</label>
-                                <div class=" col-sm-6 input-group">
-                                    <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ttscmd"/>
-                                    <span class="input-group-btn">
-                                        <a class="btn btn-default cursor" title="Rechercher un équipement" id="modalbtn"><i class="fas fa-list-alt"></i></a>
-                                    </span>
-                                </div>
-							</div>
-                            -->
-						</div>
-
 					</fieldset>
 				</form>
 				<hr>
