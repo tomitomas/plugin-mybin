9575509e85aa95e82b1fc7a942a948fda4f25245
hugoKs3 <hugo@questroy.com>
2021-03-20 20:32:32 +0100

    notif condition

--- desktop/js/mybin.js
+++ desktop/js/mybin.js
@@ -51,8 +51,9 @@ $("body").off('click','.listCmdAction').on('click','.listCmdAction', function ()
 
 // permet d'afficher la liste des cmd Jeedom pour choisir sa commande de type "info"
 $("body").off('click','.listCmdInfo').on('click','.listCmdInfo', function () {
-  var type = $(this).attr('data-type');
-  var el = $(this).closest('.' + type).find('.expressionAttr[data-l2key=notifCondition]');
+  //var type = $(this).attr('data-type');
+  //var el = $(this).closest('.' + type).find('.expressionAttr[data-l2key=notifCondition]');
+  var el = $("body").find('.expressionAttr[data-l2key=notifCondition]');
   jeedom.cmd.getSelectModal({cmd: {type: 'info'}}, function (result) {
     el.value(result.human);
   });
