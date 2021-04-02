813822d8f9071b0f837bba2b59b83c895e7b7953
hugoKs3 <hugo@questroy.com>
2021-04-02 23:39:04 +0200

    custom images

--- core/ajax/mybin.ajax.php
+++ core/ajax/mybin.ajax.php
@@ -24,7 +24,7 @@ try {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
 
-    ajax::init();
+    ajax::init(array('uploadCustomImg'));
 
     if (init('action') == 'uploadCustomImg') {
         $id = init('id');
