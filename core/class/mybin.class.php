8c2f7fe2699632d34f7a8037c1ece7426e94d58f
hugoKs3 <hugo@questroy.com>
2021-04-03 17:41:51 +0200

    custom images

--- core/class/mybin.class.php
+++ core/class/mybin.class.php
@@ -610,7 +610,7 @@ class mybin extends eqLogic {
         if ($color == '') {
             return 'plugins/mybin/plugin_info/mybin_icon.png';
         } else {
-            return 'plugins/mybin/core/assets/'.$color.'_icon.png';
+            return $this->getColorAttr($color, 'icon_on');
         }
 	}
     
