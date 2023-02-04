408c79a31eae138930d9e739eb0c71913d77e4de
ToMtOm <tomtom.infos@gmail.com>
2023-02-04 23:37:11 +0100

    ADD tomtom generic trait

---
+++ 3rdparty/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tomitomas/plugin-mybin',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '582673e836635da7c4e75aeb49d8a1ecd0ae1a9c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tomitomas/jeedom-tools' => array(
+            'pretty_version' => '0.3',
+            'version' => '0.3.0.0',
+            'reference' => '8f7d4fe048313088f9105c3814f5148bb9fd0e8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tomitomas/jeedom-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tomitomas/plugin-mybin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '582673e836635da7c4e75aeb49d8a1ecd0ae1a9c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
